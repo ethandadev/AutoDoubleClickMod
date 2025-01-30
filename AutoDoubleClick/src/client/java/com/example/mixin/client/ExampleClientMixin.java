435752e5fdccdb87344e49a88b9ef5df907e3eb8
Ethan7080 <154119174+Ethan7080@users.noreply.github.com>
2025-01-30 23:23:24 +1000

    create main

--- AutoDoubleClick/src/client/java/com/example/mixin/client/ExampleClientMixin.java
+++ AutoDoubleClick/src/client/java/com/example/mixin/client/ExampleClientMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public class ExampleClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
+	private void run(CallbackInfo info) {
 		// This code is injected into the start of MinecraftClient.run()V
 	}
 }
